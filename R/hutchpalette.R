cd45a63347ebd03f196adc0f6073cf68a5573dcc
howardbaek <howardbaek.fh@gmail.com>
2023-05-19 22:32:54 -0700

    Initial commit

---
+++ R/hutchpalette.R
@@ -0,0 +1,4 @@
+#' Fred Hutch Palette
+#'
+#' The official colors of the Fred Hutch Cancer Center in Seattle, Washington.
+"hutch_palette"
---
+++ data-raw/hutch_palette.R
@@ -0,0 +1,9 @@
+## code to prepare `hutch_palette` dataset goes here
+library(palettes)
+
+hutch_palette <- pal_palette(
+  hutch_colors = c("#1B365D", "#00C1D5", "#AA4AC4", "#FFB500",
+            "#0A799A", "#F4F4F4",  "#FFFFFF")
+)
+
+usethis::use_data(hutch_palette, overwrite = TRUE)
